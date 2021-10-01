f7e2657420724a15ab5e481c1d7a315c07b47341
권준호 <diodekjh@naver.com>
2021-10-01 14:56:31 +0000

    211001

--- react_velopert/hello-react/src/MyComponent.js
+++ react_velopert/hello-react/src/MyComponent.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+	static defaultProps = {
+		name: '기본 이름'
+	}
+	
+	static propTypes = {
+		name: PropTypes.string,
+		age: PropTypes.number.isRequired
+	}
+
 	render(){
 		return(
 			<div>
-				I'm {this.props.name}.
+				<p>I'm {this.props.name}.</p>
+				<p>I'm {this.props.age} years old.</p>
 			</div>
 		)
 	}
